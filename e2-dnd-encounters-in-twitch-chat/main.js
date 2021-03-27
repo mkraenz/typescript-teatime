52c2aa3d7985af47bd9c77ffef5ed9cc16885698
Mirco Kraenz <kraenzmirco@gmail.com>
2021-03-27 09:14:03 +0100

    E2 cleanup random encounter script a bit

--- e2-dnd-encounters-in-twitch-chat/main.js
+++ e2-dnd-encounters-in-twitch-chat/main.js
@@ -9,90 +9,107 @@ const client = new tmi.Client({
   },
   channels: ["fadeoutsama"],
 });
-
 client.connect();
+
+const say = (text) => {
+  const channel = "#fadeoutsama";
+  console.log(text);
+  client.say(channel, text);
+};
 
 let monster = null;
 let users = []; // party
 let timerInterval = null;
+const timeTillAttackInSeconds = 20;
 
 client.on("message", (channel, tags, message, self) => {
   if (self) return; // Ignore echoed messages.
   const username = tags.username;
-  const say = (text) => client.say(channel, text);
+  const msg = message.toLowerCase();
 
   logChatToPage(tags, message);
 
   hello(message, channel, tags);
-  rollDice(channel, tags, message);
+  rollDice(tags, message);
 
-  if (message.toLowerCase() === "!ambush") {
-    const timeTillAttackInSeconds = 10;
-    monster = _.cloneDeep(monsters[_.random(monsters.length - 1)]);
-    client.say(
-      channel,
-      `⚔️ An ambush! You're party is in a ${monster.area}. A wild ${monster.name} appeared. Be prepared! The attack starts in ${timeTillAttackInSeconds} seconds. ❤️: ${monster.hp}`
-    );
-    if (!timerInterval) {
-      timerInterval = window.setInterval(() => {
-        const randomUser = users[_.random(users.length - 1)];
-        const monsterTarget = randomUser;
-        if (!monsterTarget) {
-          console.log("Battle lost");
-          say(
-            `🔥🔥🔥🔥🔥 ⚰️⚰️⚰️⚰️ Defeat! The battle is lost. The world must rely on another group of adventurers. ${monster.name} lived happily ever after.`
-          );
-        }
-        const damage = _.random(19) + 1;
-        monsterTarget.hp -= damage;
-        if (monsterTarget.hp < 0) {
-          users = users.filter((u) => u.username !== monsterTarget.username);
-          say(
-            `⚰️⚰️⚰️ Oh no! @${monsterTarget.username} has been killed by ${monster.name}`
-          );
-        }
+  if (msg === "!ambush") startAmbushRandomBattle();
 
-        console.log(user, { damage });
-        users.forEach((user) => (user.hasAttacked = false));
+  if (msg === "!join") joinBattle(username);
 
-        say(
-          `🔥 ${monster.name} dealt ${damage} damage to @${monsterTarget.username}. ${monsterTarget.username} has ${monsterTarget.hp} ❤️ left.`
-        );
-      }, timeTillAttackInSeconds * 1000);
-    }
-  }
-
-  if (message.toLowerCase() === "!join") {
-    console.log(users);
-    users.push({ username: username, hasAttacked: false, hp: 150 });
-    client.say(channel, `⚔️ ${username} joined the battle alongside you.`);
-  }
-
-  const user = users.find((u) => u.username === username);
-  if (
-    message.toLowerCase().includes("!attack") &&
-    monster &&
-    user &&
-    !user.hasAttacked
-  ) {
-    console.log(username, "attacks");
-    user.hasAttacked = true;
-    const damage = _.random(19) + 1;
-    monster.hp -= damage;
-    say(
-      `🗡️ @${username} dealt ${damage} damage to ${monster.name}. ${monster.hp} ❤️ left.`
-    );
-
-    if (monster.hp <= 0) {
-      window.clearInterval(timerInterval);
-      say(
-        `🏆🏆🏆🎉🏅 VICTORY! ${monster.name} has been struck down. @${users
-          .map((u) => u.username)
-          .join(", @")} earned x00 EXP.`
-      );
+  if (msg.includes("!attack")) {
+    const user = users.find((u) => u.username === username);
+    const canAttack = monster && user && !user.hasAttacked;
+    if (canAttack) {
+      userAttack(user);
+      if (monster.hp <= 0) winBattle();
     }
   }
 });
+
+function userAttack(user) {
+  console.log(user.username, "attacks");
+  user.hasAttacked = true;
+  const damage = _.random(19) + 1;
+  monster.hp -= damage;
+  say(
+    `🗡️ @${user.username} dealt ${damage} damage to 😈 ${monster.name}. ${monster.hp} ❤️ left.`
+  );
+}
+
+function startAmbushRandomBattle() {
+  monster = _.cloneDeep(monsters[_.random(monsters.length - 1)]);
+  say(
+    `⚔️ An ambush! You're party is in a ${monster.area}. A wild 😈 ${monster.name} appeared. Be prepared! The attack starts in ${timeTillAttackInSeconds} seconds. ❤️: ${monster.hp}`
+  );
+  if (!timerInterval) {
+    timerInterval = window.setInterval(
+      monsterAttack(),
+      timeTillAttackInSeconds * 1000
+    );
+  }
+}
+
+function winBattle() {
+  window.clearInterval(timerInterval);
+  say(
+    `🏆🏆🏆🎉🏅 VICTORY! 😈 ${monster.name} has been struck down. @${users
+      .map((u) => u.username)
+      .join(", @")} earned x00 EXP.`
+  );
+}
+
+function joinBattle(username) {
+  console.log(users);
+  users.push({ username: username, hasAttacked: false, hp: 150 });
+  say(`⚔️ ${username} joined the battle alongside you.`);
+}
+
+function monsterAttack() {
+  return () => {
+    const randomUser = users[_.random(users.length - 1)];
+    const monsterTarget = randomUser;
+    if (!monsterTarget) {
+      console.log("Battle lost");
+      say(
+        `🔥🔥🔥🔥🔥 ⚰️⚰️⚰️⚰️ Defeat! The battle is lost. The world must rely on another group of adventurers. 😈 ${monster.name} lived happily ever after.`
+      );
+    }
+    const damage = _.random(19) + 1;
+    monsterTarget.hp -= damage;
+    if (monsterTarget.hp < 0) {
+      users = users.filter((u) => u.username !== monsterTarget.username);
+      say(
+        `⚰️⚰️⚰️ Oh no! @${monsterTarget.username} has been killed by 😈 ${monster.name}`
+      );
+    }
+
+    users.forEach((user) => (user.hasAttacked = false));
+
+    say(
+      `🔥 😈 ${monster.name} dealt ${damage} damage to @${monsterTarget.username}. ${monsterTarget.username} has ${monsterTarget.hp} ❤️ left.`
+    );
+  };
+}
 
 function logChatToPage(tags, message) {
   console.log(`${tags["display-name"]}: ${message}`);
@@ -109,9 +126,8 @@ function hello(message, channel, tags) {
   }
 }
 
-function rollDice(channel, tags, message) {
-  const sayDiceRoll = (roll) =>
-    client.say(channel, `@${tags.username} rolled ${roll + 1}`);
+function rollDice(tags, message) {
+  const sayDiceRoll = (roll) => say(`@${tags.username} rolled ${roll + 1}`);
   if (message.toLowerCase() === "!d2") {
     const roll = _.random(1) + 1;
     sayDiceRoll(roll);
