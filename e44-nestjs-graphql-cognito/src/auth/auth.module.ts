181d330ae3329fca14d5e9260dea2640c681d911
Mirco Kraenz <kraenzmirco@gmail.com>
2021-12-15 18:44:44 +0100

    refactor(E50): remove seemingly useless import of PassportModule

--- e44-nestjs-graphql-cognito/src/auth/auth.module.ts
+++ e44-nestjs-graphql-cognito/src/auth/auth.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { CognitoJwtStrategy } from './cognito-jwt.strategy';
 import { getJwksAsPem } from './getJWKS';
 
 @Module({
-  // TODO do we need the PassportModule?
-  imports: [PassportModule],
   providers: [
     {
       provide: 'JWKS',
