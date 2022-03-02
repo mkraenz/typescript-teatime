40485b418845d9d85d3cce82bd8e65ee25353319
Mirco Kraenz <kraenzmirco@gmail.com>
2022-03-02 20:06:36 +0100

    refactor(E57): switch to postgres

--- e44-nestjs-graphql-cognito/src/app.module.ts
+++ e44-nestjs-graphql-cognito/src/app.module.ts
@@ -2,19 +2,26 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { AuthModule } from './auth/auth.module';
 import { CustomAuthGuard } from './auth/custom-auth.guard';
 import { OrdersModule } from './orders/orders.module';
 import { TeasModule } from './teas/teas.module';
 
+const typeOrmConfig: PostgresConnectionOptions = {
+  type: 'postgres' as const,
+  synchronize: true,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  port: 3142,
+  host: 'localhost',
+  username: 'postgres',
+  password: 'password',
+  database: 'postgres',
+};
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      synchronize: true,
-      database: './db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
